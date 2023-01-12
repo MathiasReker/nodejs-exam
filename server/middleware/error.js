860d60ed388e47ec9447cce7f49ebaa32d3616a8
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2023-01-12 15:54:43 +0100

    update lock

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,15 +1,5 @@
 export const notFound = (req, res) => {
   res.status(404).send({
-    error: 'Not found',
-  });
-};
-
-export const handler = (req, res) => {
-  if (process.env.NODE_ENV === 'dev') {
-    return;
-  }
-
-  res.status(500).send({
-    error: 'Something broke. Try again later',
+    errors: ['Not found'],
   });
 };
